52da78aef222f43f7fcdea289166de58abec047f
Kwabena Agyeman <kagyeman@ymail.com>
2021-09-18 18:30:06 +0000

    added react-router-dom and react router-bootstrap and set up basic navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
 /** @format */
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header.component";
 import Footer from "./components/Footer.component";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
     <>
-      <Header />
-      <Container>
+      <Router>
+        <Header />
         <main className='py-3'>
-          <HomeScreen />
+          <Container>
+            <Route exact path='/' component={HomeScreen} />
+            <Route exact path='/product/:id' component={ProductScreen} />
+          </Container>
         </main>
-      </Container>
-      <Footer />
+        <Footer />
+      </Router>
     </>
   );
 };
