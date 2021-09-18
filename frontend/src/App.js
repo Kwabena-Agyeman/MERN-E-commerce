4e62420944cb9b67694fb7dda2751b22ef1c8000
Kwabena Agyeman <kagyeman@ymail.com>
2021-09-18 12:52:02 +0000

    added nav bar, homescreen, product and ratings components with a bit of css stylins. Also installed react- bootstrap and added bootswatch theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 /** @format */
+import { Container } from "react-bootstrap";
+import Header from "./components/Header.component";
+import Footer from "./components/Footer.component";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (
     <>
-      <h1>Welcome To ProShop</h1>
+      <Header />
+      <Container>
+        <main className='py-3'>
+          <HomeScreen />
+        </main>
+      </Container>
+      <Footer />
     </>
   );
 };
