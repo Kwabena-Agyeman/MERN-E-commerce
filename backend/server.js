8a6976316ed761c66e6565c4ee95a873e37982b2
Kwabena Agyeman <kagyeman@ymail.com>
2021-09-20 17:16:37 +0000

    finished with concurrently and added mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 /** @format */
 
-const express = require("express");
-const dotenv = require("dotenv");
-const products = require("./data/products");
+import express from "express";
+import dotenv from "dotenv";
+import products from "./data/products.js";
 
 dotenv.config();
 
