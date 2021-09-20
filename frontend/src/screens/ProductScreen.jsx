612bd8eff53c7c21257c8eed39e436424f581485
Kwabena Agyeman <kagyeman@ymail.com>
2021-09-20 15:16:57 +0000

    added nodemon and concurrently package

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,16 +1,26 @@
 /** @format */
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
-import products from "../products";
+import axios from "axios";
+
 import Ratings from "../components/Ratings.component";
 
 const ProductScreen = () => {
+  const [product, setProduct] = useState({});
   const params = useParams();
-  const product = products.find((product) => {
-    return product._id === params.id;
-  });
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const res = await axios.get(`/api/products/${params.id}`);
+
+      setProduct(res.data);
+    };
+
+    fetchProduct();
+  }, []);
+
   return (
     <>
       <Link className='btn btn-light my-3' to='/'>
